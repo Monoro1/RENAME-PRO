c3f5d8d7637f208b8e9fc846aa143ec83ab90a3f
Monoro1 <animebotttt@gmail.com>
2024-07-14 12:56:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7408090103:AAEo-PH2M20qbnplPbdjFKMhZAti-rZnA_s")
+API_ID = int(os.environ.get("API_ID", "29489981"))
+API_HASH = os.environ.get("API_HASH", "d06d320353dd369c577b33be893c622b")
+OWNER = int(os.environ.get("OWNER", "7063650557"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Filexrenamer_jks_bot")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002153242485"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://anime578901:anime12345678@cluster0.ytgrmvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "anime578901")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
