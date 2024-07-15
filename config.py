c3df801c10e8bb8c26ff49952565f5c774ffd7b7
Monoro1 <animebotttt@gmail.com>
2024-07-15 11:54:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002153242485"))
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://anime578901:anime12345678@cluster0.ytgrmvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "anime578901")
 
-STRING = os.environ.get("STRING", "BQHB-z0ARfVmpR7l9uMGqCGJNQ3GeUIc6mvRbVC8mkkgEc_N32Gm_7SDOhEyciMz-cnFgKuQobtua_3DiF-gY1hZ38CYUUaujqqo93H3uZDvflopUXdf9iY24HWMdQZ7fBmIbQxedOH61L_9V5-18Xf3R9-Bk6iIdIpLApBk6uxntKaE7PvbQd_IlBfH4FOPXiDf11jxewaw_H8SE-F1H7-wx6D7zIw_PRITv9zifX9cGLaidHY9l9MfFVXIZ_a5NHJBylq43Z3Mm7L-a1cMTEBH8ndryW-jTyiaTy4tEFKLhOVZrQC-MTwnHjckZdc_q5jGGE37NMps8yGDaTLy6NcFwfaNjwAAAAE7x1xoAA")
+STRING = os.environ.get("STRING", "BQHB-z0AiLvhMHdZuOgeJ1gt6ljCWSrb_IUTU2zMDM4wI9_jdD9LSz3D5EvpdUiM1ONEc2LxsXqAauzXnZHB8qDbpuiV7CKR_lA5-nDPKtGRQKxtrjVbSIct326ocLyTsyrCArCBQYO2m0hjBKNY1NOnMyjLf0rsER8V1PC2i4TWlr6wHdcwx4MChMAAh5y9-dDBAhf0f4gXg5kr7mAthPEzmQye7yp-IhTShNl9WO2Vtwe1KhD5ZOFUH4Qig-iIqgZwjFIuYCcDA5Gxw2IRVIeadOEDdTDlCJ8JG4W7zLJ6lzqSXSmzu4hin-P31KK6avkGZ0evsuMmO79N1JNILU67QEaSlAAAAAE7x1xoAA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
 
 
