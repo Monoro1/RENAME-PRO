b55878531a2945da398c863d87aed5ae8180a5c9
Monoro1 <animebotttt@gmail.com>
2024-07-15 11:16:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002153242485"))
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://anime578901:anime12345678@cluster0.ytgrmvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "anime578901")
 
-STRING = os.environ.get("STRING", "False")
+STRING = os.environ.get("STRING", "BQHB-z0ARfVmpR7l9uMGqCGJNQ3GeUIc6mvRbVC8mkkgEc_N32Gm_7SDOhEyciMz-cnFgKuQobtua_3DiF-gY1hZ38CYUUaujqqo93H3uZDvflopUXdf9iY24HWMdQZ7fBmIbQxedOH61L_9V5-18Xf3R9-Bk6iIdIpLApBk6uxntKaE7PvbQd_IlBfH4FOPXiDf11jxewaw_H8SE-F1H7-wx6D7zIw_PRITv9zifX9cGLaidHY9l9MfFVXIZ_a5NHJBylq43Z3Mm7L-a1cMTEBH8ndryW-jTyiaTy4tEFKLhOVZrQC-MTwnHjckZdc_q5jGGE37NMps8yGDaTLy6NcFwfaNjwAAAAE7x1xoAA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
 
 
